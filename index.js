1e122c000487953c627c6bc63195fc31cc3f4e63
vic3214 <45560017+vic3214@users.noreply.github.com>
2021-03-22 12:42:15 +0100

    Corrección de rutas

--- index.js
+++ index.js
@@ -8,23 +8,20 @@ var port = (process.env.PORT || 10000);
 
 const path = require("path");
 
-app.use("/", express.static(path.join(__dirname, "public")));
-
 app.get("/cool",(request,response) =>{
     response.send(cool());
     console.log("New request to /cool has arrived");
 });
 
-app.get("/public/index.html",(request,response) =>{
-    response.sendFile(path.join(__dirname,"public/index.html"));
+app.use("/",express.static(path.join(__dirname,"public")));
+
+app.get("/index.html",(request,response) =>{
+    response.sendFile(path.join(__dirname,"/index.html"));
     console.log("File sent");
 });
 
 app.listen(port,()=>{
     console.log("Server ready listening on port "+port);
 });
-
-
-
 
 console.log(cool());
