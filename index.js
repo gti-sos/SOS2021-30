07856fb1ab11a56991601b548f2a4f1b53226f4e
Alejandro Sánchez Márquez <alejandrosanchezmarquez10@gmail.com>
2021-03-22 22:11:41 +0100

    añadido /info/life-expectancy-stats a index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/index.html",(request,response) =>{
 });
 
 app.get("/info/life-expectancy-stats",(request,response) =>{
-    response.send("<html><body>aes</body></html>");
+    response.send("<html><body><p>Datos de la esperanza de vida en España por provincia/año/media mujeres/media hombres/media conjunta</p><table border='1'><tr><td>country</td><td>province</td><td>year</td><td>life-expectancy-woman</td><td>life-expentancy-man</td><td>average-life-expectancy</td></tr><tr><td>España</td><td>Andalucia</td><td>2017</td><td>84,41</td> <td>79,23</td><td>81,63</td> </tr><tr><td>España</td><td>Aragón</td><td>2017</td><td>86,06</td> <td>80,43</td><td>83,23</td></tr><tr><td>España</td><td>Asturias</td><td>2017</td><td>85.51</td><td>79,50</td><td>82,58</td></tr><tr><td>España</td><td>Cantabria</td><td>2017</td><td>86,22</td><td>80,34</td><td>83,33</td></tr><tr><td>España</td><td>Ceuta</td><td>2017</td><td>82,11</td><td>76,65</td><td>79,31</td></tr></table></body></html>");
     console.log("Info sent");
 });
 
