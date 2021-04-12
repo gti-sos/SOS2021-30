cafde918afe10a2ee1055d7c43071aa8c67f0b32
Alejandro Sánchez Márquez <alejandrosanchezmarquez10@gmail.com>
2021-04-12 21:38:16 +0200

    modificados metodos de life-expectancy-stats

--- index.js
+++ index.js
@@ -11,15 +11,11 @@ const path = require("path");
 //RUTA BASE DE LA API
 var BASE_API_PATH = "/api/v1";
 
-<<<<<<< HEAD
 //~~~~~~~~~~~~~~~~~~~~~~~~ API REST WEIGHTS-STATS ~~~~~~~~~~~~~~~~~~~~~~~~
 var weightsStatsAPI = require("./weightsStatsAPI");
 weightsStatsAPI.register(app, BASE_API_PATH);
 //~~~~~~~~~~~~~~~~~~~ END: API REST WEIGHTS-STATS ~~~~~~~~~~~~~~~~~~~~~~~~
 
-=======
-const path = require("path");
->>>>>>> 6e0e85352f7b096e83f546c80ef7faa9b6057d91
 
 app.use(bodyParser.json());
 
@@ -52,10 +48,6 @@ app.get("/info/alcohol-consumption-stats",(request,response) =>{
     console.log("Info about alcohol-consumption-stats sent");
 });
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 6e0e85352f7b096e83f546c80ef7faa9b6057d91
  var alcoholConsumptionStats=[];
  
 app.get(BASE_API_PATH+"/alcohol-consumption-stats/loadInitialData",(req,res)=>{
@@ -436,7 +428,6 @@ var lifeExpectancyStats=[];
 app.get(BASE_API_PATH+"/life-expectancy-stats/loadInitialData",(req,res)=>{
     lifeExpectancyStats=[
         {
-            "id":1,
             "country":"España",
             "province":"Andalucia",
             "year":"2017",
@@ -445,7 +436,6 @@ app.get(BASE_API_PATH+"/life-expectancy-stats/loadInitialData",(req,res)=>{
             "averageLifeExpectancy":"81,63"
         },
         {
-            "id":2,
             "country":"España",
             "province":"Aragón",
             "year":"2017",
@@ -454,7 +444,6 @@ app.get(BASE_API_PATH+"/life-expectancy-stats/loadInitialData",(req,res)=>{
             "averageLifeExpectancy":"83,23"
         },
         {
-            "id":3,
             "country":"España",
             "province":"Asturias",
             "year":"2017",
@@ -480,58 +469,64 @@ app.post(BASE_API_PATH+"/life-expectancy-stats",(req,res)=>{
     res.sendStatus(201);
 });
 //GET A UN RECURSO 
-app.get(BASE_API_PATH+"/life-expectancy-stats/:id",(req,res)=>{
-    const {id} = req.params;
+app.get(BASE_API_PATH+"/life-expectancy-stats/:province/:year",(req,res)=>{
+    const {province,year} = req.params;
+    console.log(province);
+    console.log(year);
     _.each(lifeExpectancyStats,(lifeExpectancyStats,i)=>{
-        if(lifeExpectancyStats.id==id){
+        if(lifeExpectancyStats.province==province && lifeExpectancyStats.year==year){
             res.send(JSON.stringify(lifeExpectancyStats,null,2));
-        }
-    });
-    res.sendStatus(200);
-});
-
-
-//DELETE A UN RECURSO
-app.delete(BASE_API_PATH+"/life-expectancy-stats/:id",(req,res)=>{
-    const {id} = req.params;
-    _.each(lifeExpectancyStats,(lifeExpectancyStats,i)=>{
-        if(lifeExpectancyStats.id==id){
-            lifeExpectancyStats.splice(i,1);
-            res.send(lifeExpectancyStats);
             res.sendStatus(200);
         }else{
             res.sendStatus(404);
         }
     });
+    
+});
+
+
+//DELETE A UN RECURSO
+app.delete(BASE_API_PATH +"/life-expectancy-stats/:province/:year", (req, res) =>{ 
+    const {province,year} = req.params;
+
+    for (var i = 0; i <  lifeExpectancyStats.length; i++){
+		if(lifeExpectancyStats[i].province == province && lifeExpectancyStats[i].year == year){
+			lifeExpectancyStats.splice(i,1);
+			return res.sendStatus(200);
+		}
+	}
+	res.sendStatus(404);
 });
 
 //PUT A UN RECURSO
-app.put(BASE_API_PATH+"/life-expectancy-stats/:id",(req,res)=>{
-    const {id} = req.params;
-    const {country,province,year,lifeExpectancyWoman,lifeExpentancyMan,averageLifeExpectancy}=req.body;
-    if(country&&province&&year&&lifeExpectancyWoman&&lifeExpentancyMan&&averageLifeExpectancy){
-        _.each(lifeExpectancyStats,(lifeExpectancyStats,i)=>{
-            if(lifeExpectancyStats.id==id){
-                lifeExpectancyStats.province=country;
-                lifeExpectancyStats.province=province;
-                lifeExpectancyStats.year=year;
-                lifeExpectancyStats.lifeExpectancyWoman=lifeExpectancyWoman;
-                lifeExpectancyStats.lifeExpentancyMan=lifeExpentancyMan;
-                lifeExpectancyStats.averageLifeExpectancy=averageLifeExpectancy;
+app.put(BASE_API_PATH+"/life-expectancy-stats/:province/:year",(req,res)=>{
+    const {province,year} = req.params;
+    const {country,provinceData,yearData,lifeExpectancyWoman,lifeExpentancyMan,averageLifeExpectancy}=req.body;
+        for (var i = 0; i <  lifeExpectancyStats.length; i++){
+            if(lifeExpectancyStats[i].province==province && lifeExpectancyStats[i].year==year){
+                lifeExpectancyStats[i].country=country;
+                lifeExpectancyStats[i].province=province;
+                lifeExpectancyStats[i].year=year;
+                lifeExpectancyStats[i].lifeExpectancyWoman=lifeExpectancyWoman;
+                lifeExpectancyStats[i].lifeExpentancyMan=lifeExpentancyMan;
+                lifeExpectancyStats[i].averageLifeExpectancy=averageLifeExpectancy;
+
+                //Envio de recurso actualizado
+                res.json(lifeExpectancyStats);
+                res.status(200);
+            }else{
+                res.status(500).json({error: 'There was an error.'});
             }
-        });
-        //Envio de recurso actualizado
-        res.json(lifeExpectancyStats);
-        res.status(200);
+        }
+
     
-    }else{
-        res.status(500).json({error: 'There was an error.'})
-    }
+        
+    
 });
 
 
 //POST A UN RECURSO (Debe dar error)
-app.post(BASE_API_PATH+"/life-expectancy-stats/:id",(req,res)=>{
+app.post(BASE_API_PATH+"/life-expectancy-stats/:province/:year",(req,res)=>{
     res.sendStatus(405);
 });
 
