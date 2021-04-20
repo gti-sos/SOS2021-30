3dfa7b9921b3ff0189d18cde4318a7e14a486c4c
juanpepitt <juanpepitt@gmail.com>
2021-04-20 17:49:30 +0200

    Modificaciones para F06

--- smokersStatsAPI/index.js
+++ smokersStatsAPI/index.js
@@ -141,10 +141,7 @@ module.exports.register = (app) => {
             if (err){
                 console.log("ERROR accesing DB in POST: "+err);
                 res.sendStatus(500);
-            }else if (req.body == 8){
-                res.sendStatus(400);
-            }
-            else{
+            }else{
                 }if(dataInDB.length == 0 && newData.length == 8){
                     console.log(`Inserting new data in DB: <${JSON.stringify(newData,null,2)}>.`);
                     db.insert(newData);
