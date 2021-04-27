e653283dd79430a4f3fb027d74bc8daf99857290
juanpepitt <juanpepitt@gmail.com>
2021-04-27 14:09:46 +0200

    inclusión de Smokers

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,7 @@
 	import Router from 'svelte-spa-router';
   import LifeExpectancy from './lifeExpectancy.svelte';
 	import TablaAlcohol from "./TablaAlcohol.svelte";
+  import SmokerStats from "./Smokers.svelte";
   import FrontWeight from "./Weights.svelte";
 	import {Navbar,NavbarBrand,Nav,NavItem,NavLink } from 'sveltestrap';
 
@@ -14,6 +15,7 @@
 		"/alcohol-stats": TablaAlcohol,
     "/life-stats" : LifeExpectancy,
     "/weights-stats" : FrontWeight,
+    "/smokers-stats" : SmokerStats,
 		'*': NotFound};
 </script>
 
@@ -27,6 +29,9 @@
               </NavItem>
               <NavItem>
                 <NavLink href="#/alcohol-stats">Tabla alcohol</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#/smokers-stats">Tabla fumadores</NavLink>
               </NavItem>
               <NavItem>
                 <NavLink href="#/life-stats">Tabla esperanza de vida</NavLink>
---
+++ src/Smokers.svelte
@@ -0,0 +1,75 @@
+<script>
+    import{ 
+        onMount
+
+    } from "svelte";
+
+    import Table from "sveltestrap/src/Table.svelte";
+
+    let SmokerStats = [];
+
+    async function getSmokers(){
+        console.log("Fetching resources...");
+        const res = await fetch("/api/v1/smokers-stats/loadInitialData");
+
+        if(res.ok){
+            console.log("Ok.");
+            const json = await res.json();
+            Smokers = json;
+            console.log(`We have received ${SmokerStats.lenght} resources`);
+        }else{
+            console.log("Error!");
+        }
+    }
+
+    onMount(getSmokers);
+
+    console.log("Before getSmokers");
+    getSmokers();
+    console.log("After getSmokers");
+
+
+
+
+
+</script>
+
+<main>
+    <body>
+ 
+        <h1>Tabla sobre fumadores</h1>
+
+        <Table bordered responsive> 
+            <thead>
+                <tr>
+                    <td>Pais</td>
+                    <td>Comunidad autonoma</td>
+                    <td>Año</td>
+                    <td>Fumadores diarios</td>
+                    <td>Fumadores ocasionales</td>
+                    <td>Ex-Fumadores</td>
+                    <td>No Fumadores</td>
+                </tr>
+        </thead>
+        <tbody>
+        {#each SmokerStats as smokers}
+            <tr>
+                <td>{smokers.country}</td>
+                <td>{smokers.province}</td>
+                <td>{smokers.year}</td>
+                <td>{smokers.dailySmoker}</td>
+                <td>{smokers.ocasionalSmoker}</td>
+                <td>{smokers.exSmoker}</td>
+                <td>{smokers.nonSmoker}</td>
+        </tr>
+                
+            {/each}
+        </tbody>
+        </Table>
+    </body>
+</main>
+
+
+<style>
+
+</style>
