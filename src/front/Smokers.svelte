3a6b7e072af529722028c2848255212d67eeb512
juanpepitt <juanpepitt@gmail.com>
2021-05-03 01:47:41 +0200

    Reorganización del proyecto y modificación de index.js

--- src/front/Smokers.svelte
+++ src/front/Smokers.svelte
@@ -284,7 +284,7 @@
                     <td><input type = "number" placeholder="0000" bind:value="{newSmoker.ocasionalSmoker}"></td>    
                     <td><input type = "number" placeholder="0000" bind:value="{newSmoker.exSmoker}"></td>  
                     <td><input type = "number" placeholder="0000" bind:value="{newSmoker.nonSmoker}"></td>  
-                    <td><Button outline color="primary" on:click={insertSmokers} colspan="2">Insertar</Button></td>          
+                    <td colspan="2"><Button outline color="primary" on:click={insertSmokers}>Insertar</Button></td>          
                 </tr>
  
                 {#each SmokerStats as sc}
@@ -307,10 +307,10 @@
         <Button color="danger" on:click="{deleteALL}">
             Eliminar todo
         </Button>
-        <Button style ="text-align: right;" outline color="primary" on:click="{getPreviewPage}">
+        <Button style ="align: right" outline color="primary" on:click="{getPreviewPage}">
            Atrás
         </Button>
-        <Button style ="text-align: right;" outline color="primary" on:click="{getNextPage}">
+        <Button style ="align: right" outline color="primary" on:click="{getNextPage}">
             Siguiente
          </Button>
          
