d422b1dc8becee498a8c6defa82eb1a073d68b78
juanpepitt <juanpepitt@gmail.com>
2021-05-25 18:38:03 +0200

    Integraciones inicial

--- src/front/smokersStats/integrations/ArmsSales.svelte
+++ src/front/smokersStats/integrations/ArmsSales.svelte
@@ -2,18 +2,16 @@
     import react from 'react';
     import { JSCharting } from 'jscharting-react';
 
-("chartDiv", {
-  series: [
-    {
-      points: [{ x: "A", y: 10 }, { x: "B", y: 5 }]
-    }
-  ]
+    const chart = new JSC.Chart('chartDiv', { 
+  series:[{
+    points: [{ x: 'A', y: 10 }, { x: 'B', y: 5 }]
+  }], 
 });
 
 </script>
 
 <main>
     
-<div id="chartDiv" style="width: 100%; height: 400px;"></div>
+    <div id="chartDiv" style="width: 100%; height: 400px;"></div>
 
 </main>
