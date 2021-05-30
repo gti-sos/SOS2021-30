69380584fd204f857e51933fb40d2cc146a75fa0
juanpepitt <juanpepitt@gmail.com>
2021-05-30 22:25:17 +0200

    Modificaciones de integración y preparación D03

--- src/back/smokersStatsAPI/v2/index.js
+++ src/back/smokersStatsAPI/v2/index.js
@@ -437,7 +437,7 @@ module.exports.register = (app) => {
     //PROXY
     app.use(BASE_API_PATH+"/proxySmoker", function(req, res) {
         console.log("New proxy call");
-        var apiServerHost = 'https://sos2021-30.herokuapp.com'+BASE_API_PATH;
+        var apiServerHost = "https://sos2021-30.herokuapp.com"+BASE_API_PATH;
         var url = apiServerHost + req.url;
         console.log("piped: /proxyHeroku -> " + url);
 
