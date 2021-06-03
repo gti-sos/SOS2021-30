e5fea6f7db039539f01bfd6cfe5fbc3d6b9e5da8
Jesus Ruiz <ruiz.dominguez.jesus@gmail.com>
2021-06-03 13:00:51 +0200

    integracion externa 3

--- src/front/weightsStats/integrations/integracionExt3.svelte
+++ src/front/weightsStats/integrations/integracionExt3.svelte
@@ -1,6 +1,7 @@
 <script>
     import { pop }from "svelte-spa-router";
     import Button from "sveltestrap/src/Button.svelte";
+    import anychart from "anychart";
 
 
     const BASE_FUTBOL_PATH = "https://apiv3.apifootball.com/?action=get_standings&league_id=302&APIkey=238fa8f2f748e83ca2569474a8f848897110a90bb6837bd17d6cdc66a02eb14a";
@@ -11,7 +12,7 @@
     let golesFavor = [];
     let golesContra = [];
 
-    async function loadGraph(){
+    anychart.onDocumentReady(async function(){
         console.log("Fetching data...");
         const res = await fetch(BASE_FUTBOL_PATH);
         futbolData = await res.json();
@@ -23,9 +24,7 @@
                 golesFavor.push(stat["overall_league_GF"]);
                 golesContra.push(stat["overall_league_GA"]);
             });
-        }
-
-        
+        }        
 
         console.log("Generando datos...");
         console.log("We have " + equipo.length + " teams in the league");
@@ -33,40 +32,52 @@
         console.log("We hace " + golesFavor.length + " datas from the goals of each team");
         console.log("We hace " + golesContra.length + " datas from the goals against each team");
 
-        new Morris.Bar({
-            element: 'myfirstchart',
-            //Tratamiento de datos de la integración manual
-            data: [
-                { equipo: "Atletico de Madrid", value: puntos[0] , value2: golesFavor[0], value3: golesContra[0]},
-                { equipo: "Madrid", value: puntos[1] , value2: golesFavor[1], value3: golesContra[1]},
-                { equipo: "Barcelona", value: puntos[2] , value2: golesFavor[2], value3: golesContra[2]},
-                { equipo: "Sevilla", value: puntos[3] , value2: golesFavor[2], value3: golesContra[3]},
-            ],
-            xkey: 'equipo',
-            ykeys: ['value', 'value2', 'value3'],
-            labels: ['Puntos', 'Goles a favor', 'Goles en contra']
-            });
-
-    }
-     
+        //Tratamiento de los datos integrados de la API a mano
+            var chart = anychart.column3d([
+                {x: equipo[0], value: puntos[0], value2: golesFavor[0]},
+                {x: equipo[1], value: puntos[1], value2: golesFavor[1]},
+                {x: equipo[2], value: puntos[2], value2: golesFavor[2]},
+                {x: equipo[3], value: puntos[3], value2: golesFavor[3]},
+                {x: equipo[4], value: puntos[4], value2: golesFavor[4]},
+                {x: equipo[5], value: puntos[5], value2: golesFavor[5]},
+                {x: equipo[6], value: puntos[6], value2: golesFavor[6]},
+                {x: equipo[7], value: puntos[7], value2: golesFavor[7]},
+                {x: equipo[8], value: puntos[8], value2: golesFavor[8]},
+                {x: equipo[9], value: puntos[9], value2: golesFavor[9]},
+                {x: equipo[10], value: puntos[10], value2: golesFavor[10]},
+                {x: equipo[11], value: puntos[11], value2: golesFavor[11]},
+                {x: equipo[12], value: puntos[12], value2: golesFavor[12]},
+                {x: equipo[13], value: puntos[13], value2: golesFavor[13]},
+                {x: equipo[14], value: puntos[14], value2: golesFavor[14]},
+                {x: equipo[15], value: puntos[15], value2: golesFavor[15]},
+                {x: equipo[16], value: puntos[16], value2: golesFavor[16]},
+                {x: equipo[17], value: puntos[17], value2: golesFavor[17]},
+                {x: equipo[18], value: puntos[18], value2: golesFavor[18]},
+                {x: equipo[19], value: puntos[19], value2: golesFavor[19]}
+            ]);
+            chart.title("Estadísitcas liga española de fútbol");
+            chart.container("container");
+            chart.draw();
+        });
+ 
 </script>
 
 <svelte:head>
 
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js" on:load="{loadGraph}"></script>
+    <script src="https://cdn.anychart.com/releases/v8/js/anychart-base.min.js"></script>
+    <script src="https://cdn.anychart.com/releases/v8/js/anychart-ui.min.js"></script>
+    <script src="https://cdn.anychart.com/releases/v8/js/anychart-exports.min.js"></script>
+    <link href="https://cdn.anychart.com/releases/v8/css/anychart-ui.min.css" type="text/css" rel="stylesheet">
+    <link href="https://cdn.anychart.com/releases/v8/fonts/css/anychart-font.min.css" type="text/css" rel="stylesheet">
 
 </svelte:head>
 
-
 <main>
-    <h1 style="text-align: center">Estadística de obesidad integradas con el estrés</h1>
-    <div id="myfirstchart" style="height: 250px;"></div>
+    <div id="container" />
     <p>Gráfico que muestra el resultado de la liga española indicando los puntos, goles a favor y goles en contra de cada equipo.</p>
 
   <p>Datos obtenidos de: <a href="https://apifootball.com/">https://apifootball.com/</a></p>
+  <h7 style="color: gray;">Gráfica diseñada con anychart</h7> 
   <Button outline color="secondary" on:click="{pop}">Atrás</Button>
  
 </main>
