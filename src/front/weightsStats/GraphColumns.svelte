0c109e2929d56025858e08280e3e1232d954a109
Jesus Ruiz <ruiz.dominguez.jesus@gmail.com>
2021-06-05 11:30:49 +0200

    tipo column cambiado por cilinder para que no haya repeticiones

--- src/front/weightsStats/GraphColumns.svelte
+++ src/front/weightsStats/GraphColumns.svelte
@@ -25,29 +25,25 @@
         }
 
         console.log("Generando datos...");
-        // Set up the chart
-        const chart = new Highcharts.Chart({
+        Highcharts.chart('container', {
             chart: {
-                renderTo: 'container',
-                type: 'column',
+                type: 'cylinder',
                 options3d: {
                     enabled: true,
-                    alpha: 0,
+                    alpha: 15,
                     beta: 15,
                     depth: 50,
-                    viewDistance: 25
+                    viewDistance: 35
                 }
             },
             title: {
                 text: 'Gráfica IMC por comunidades'
             },
-
             yAxis: {
                 title: {
                     text: 'Porcentaje'
                 }
             },
-
             xAxis: {
                 title: {
                     text: 'Comunidad autónoma/año'
@@ -55,10 +51,11 @@
                 categories: weightChartInfo
             },
             plotOptions: {
-                column: {
-                    depth: 25
+                series: {
+                    depth: 25,
+                    colorByPoint: true
                 }
-            },
+            },            
             series: [
             {
             name: 'Peso normal',
@@ -74,20 +71,6 @@
             ]
         });
 
-        function showValues() {
-            document.getElementById('alpha-value').innerHTML = chart.options.chart.options3d.alpha;
-            document.getElementById('beta-value').innerHTML = chart.options.chart.options3d.beta;
-            document.getElementById('depth-value').innerHTML = chart.options.chart.options3d.depth;
-        }
-
-        // Activate the sliders
-        document.querySelectorAll('#sliders input').forEach(input => input.addEventListener('input', e => {
-            chart.options.chart.options3d[e.target.id] = parseFloat(e.target.value);
-            showValues();
-            chart.redraw(false);
-        }));
-
-        showValues();
     }
      
 </script>
@@ -96,6 +79,7 @@
 
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/highcharts-3d.js"></script>
+    <script src="https://code.highcharts.com/modules/cylinder.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/export-data.js"></script>
     <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
@@ -108,27 +92,18 @@
     <figure class="highcharts-figure">
         <div id="container"></div>
         <p class="highcharts-description">
-            Gráfico de barras 3D en el que se ve representado el porcentaje por comunidades autónomas en los años 2014 y 2017 el IMC
-        </p>
-        <div id="sliders">
-            <table>
-                <tr>
-                    <td><label for="alpha">Alpha Angle</label></td>
-                    <td><input id="alpha" type="range" min="0" max="45" value="15"/> <span id="alpha-value" class="value"></span></td>
-                </tr>
-                <tr>
-                    <td><label for="beta">Beta Angle</label></td>
-                    <td><input id="beta" type="range" min="-45" max="45" value="15"/> <span id="beta-value" class="value"></span></td>
-                </tr>
-                <tr>
-                    <td><label for="depth">Depth</label></td>
-                    <td><input id="depth" type="range" min="20" max="100" value="50"/> <span id="depth-value" class="value"></span></td>
-                </tr>
-            </table>
-        </div>
+            Gráfico de cilindros 3D en el que se ve representado el porcentaje por comunidades 
+            autónomas en los años 2014 y 2017 el IMC</p>
     </figure>
     
-    <h7 style="color: gray;">Gráfica diseñada con Highcharts - type: 'column'</h7>
+    <h7 style="color: gray;">Gráfica diseñada con Highcharts - type: 'cylinder'</h7>
     <Button outline color="secondary" on:click="{pop}">Atrás</Button>
  
 </main>
+
+<style>
+#container {
+    height: 650px; 
+    width: 2200px;
+}
+</style>
